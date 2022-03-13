f1bc2c9dd14d7cc5e2fed2de2115077e539e0ba8
Simon Condon <simoncondon@hotmail.com>
2022-03-13 19:16:24 +0000

    Added book version of unification algorithm to benchmark project

--- src/SCFirstOrderLogic.Benchmarks/AlternativeImplementations/FromAiAModernApproach/SentenceUnifier.cs
+++ src/SCFirstOrderLogic.Benchmarks/AlternativeImplementations/FromAiAModernApproach/SentenceUnifier.cs
@@ -1,216 +1,208 @@
-﻿////using System;
-////using System.Collections.Generic;
-////using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-////namespace SCFirstOrderLogic.SentenceManipulation
-////{
-////    /*
-////     * function UNIFY(x, y, θ) returns a substitution to make x and y identical
-////     *   inputs:
-////     *     x, a variable, constant, list, or compound expression
-////     *     y, a variable, constant, list, or compound expression
-////     *     θ, the substitution built up so far (optional, defaults to empty)
-////     *   
-////     *   if θ = failure then return failure
-////     *   else if x = y then return θ
-////     *   else if VARIABLE?(x) then return UNIFY-VAR(x, y, θ)
-////     *   else if VARIABLE?(y) then return UNIFY-VAR(y, x, θ)
-////     *   else if COMPOUND?(x) and COMPOUND?(y) then
-////     *     return UNIFY(x .ARGS, y.ARGS, UNIFY(x.OP, y.OP, θ))
-////     *   else if LIST?(x) and LIST?(y) then
-////     *     return UNIFY(x .REST, y.REST, UNIFY(x.FIRST, y.FIRST, θ))
-////     *   else return failure
-////     *   
-////     * function UNIFY-VAR(var, x, θ) returns a substitution
-////     * if {var/val} ∈ θ then return UNIFY(val, x, θ)
-////     * else if {x/val} ∈ θ then return UNIFY(var, val, θ)
-////     * else if OCCUR-CHECK?(var, x) then return failure
-////     * else return add {var /x} to θ
-////     *
-////       Russell, Stuart; Norvig, Peter. Artificial Intelligence: A Modern Approach, Global Edition (p. 328). Pearson Education Limited. Kindle Edition. 
-////     */
-////    public class SentenceUnifier
-////    {
-////        public bool Unify(Sentence x, Sentence y, out IDictionary<Variable, Term> unifier)
-////        {
-////            unifier = new Dictionary<Variable, Term>();
+namespace SCFirstOrderLogic.Benchmarks.AlternativeImplementations.FromAiAModernApproach
+{
+    /// <summary>
+    /// Sentence unification logic implemented as close as possible to the way it
+    /// is stated in figure 9.1 of Artificial Intelligence: A Modern Approach.
+    /// For learning outcomes only - obviously not a great way to implement this in C#.
+    /// </summary>
+    public static class SentenceUnifier
+    {
+        /*
+         * function UNIFY(x, y, θ) returns a substitution to make x and y identical
+         *   inputs:
+         *     x, a variable, constant, list, or compound expression
+         *     y, a variable, constant, list, or compound expression
+         *     θ, the substitution built up so far (optional, defaults to empty)
+         *   
+         *   if θ = failure then return failure
+         *   else if x = y then return θ
+         *   else if VARIABLE?(x) then return UNIFY-VAR(x, y, θ)
+         *   else if VARIABLE?(y) then return UNIFY-VAR(y, x, θ)
+         *   else if COMPOUND?(x) and COMPOUND?(y) then
+         *     return UNIFY(x .ARGS, y.ARGS, UNIFY(x.OP, y.OP, θ))
+         *   else if LIST?(x) and LIST?(y) then
+         *     return UNIFY(x .REST, y.REST, UNIFY(x.FIRST, y.FIRST, θ))
+         *   else return failure
+         *
+         * Figure 9.1
+         * Russell, Stuart; Norvig, Peter. Artificial Intelligence: A Modern Approach, Global Edition (p. 328). Pearson Education Limited. Kindle Edition. 
+         */
+        public static Substitution Unify(object x, object y, Substitution θ)
+        {
+            θ ??= new Substitution();
 
-////            if (TryUnify(x, y, unifier))
-////            {
-////                unifier = null;
-////                return false;
-////            }
+            if (θ == Substitution.Failure)
+                return Substitution.Failure;
+            else if (EffectivelyEqual(x, y))
+                return θ;
+            else if (IsVariable(x))
+                return UnifyVar(x, y, θ);
+            else if (IsVariable(y))
+                return UnifyVar(y, x, θ);
+            else if (IsCompound(x) && IsCompound(y))
+                return Unify(ArgsOf(x), ArgsOf(y), Unify(OperatorOf(x), OperatorOf(y), θ));
+            else if (IsList(x) && IsList(y))
+                return Unify(RestOf(x), RestOf(y), Unify(FirstOf(x), FirstOf(y), θ));
+            else
+                return Substitution.Failure;
+        }
 
-////            return true;
-////        }
+        /*
+         * function UNIFY-VAR(var, x, θ) returns a substitution
+         *   if {var/val} ∈ θ then return UNIFY(val, x, θ)
+         *   else if {x/val} ∈ θ then return UNIFY(var, val, θ)
+         *   else if OCCUR-CHECK?(var, x) then return failure
+         *   else return add {var /x} to θ
+         *
+         * Figure 9.1
+         * Russell, Stuart; Norvig, Peter. Artificial Intelligence: A Modern Approach, Global Edition (p. 328). Pearson Education Limited. Kindle Edition. 
+         */
+        private static Substitution UnifyVar(object @var, object x, Substitution θ)
+        {
+            if (θ.TryGetValue(var, out var varval))
+                return Unify(varval, x, θ);
+            else if (θ.TryGetValue(x, out var xval))
+                return Unify(@var, xval, θ);
+            ////else if (OccurCheck(var, x))
+            ////    return Substitution.Failure;
+            else
+                return θ.Add(@var, x);
+        }
 
-////        private Substitution Unify(Sentence x, Sentence y, Substitution substitution)
-////        {
-////            // TODO-PERFORMANCE: Given the fundamentality of unification and the number of times that this could be called during inference,
-////            // it might be worth optimising it a little via a visitor-style design instead of this type switch..
-////            return (x, y) switch
-////            {
-////                (Conjunction conjunctionX, Conjunction conjunctionY) => TryUnify(conjunctionX, conjunctionY, unifier),
-////                (Disjunction disjunctionX, Disjunction disjunctionY) => TryUnify(disjunctionX, disjunctionY, unifier),
-////                (Equality equalityX, Equality equalityY) => TryUnify(equalityX, equalityY, unifier),
-////                (Equivalence equivalenceX, Equivalence equivalenceY) => TryUnify(equivalenceX, equivalenceY, unifier),
-////                //(ExistentialQuantification existentialQuantificationX, ExistentialQuantification existentialQuantificationY) => TryUnify(existentialQuantificationX, existentialQuantificationY, unifier),
-////                (Implication implicationX, Implication implicationY) => TryUnify(implicationX, implicationY, unifier),
-////                (Negation negationX, Negation negationY) => TryUnify(negationX, negationY, unifier),
-////                (Predicate predicateX, Predicate predicateY) => TryUnify(predicateX, predicateY, unifier),
-////                //(UniversalQuantification universalQuantificationX, UniversalQuantification universalQuantificationY) => TryUnify(universalQuantificationX, universalQuantificationY, unifier),
-////                _ => false
-////            };
-////        }
+        private static bool EffectivelyEqual(object x, object y)
+        {
+            return (x, y) switch
+            {
+                (Constant cx, Constant cy) => cx.Symbol.Equals(cy.Symbol),
+                (VariableReference vx, VariableReference vy) => vx.Declaration.Symbol.Equals(vy.Declaration.Symbol),
+                (List<object> lx, List<object> ly) => lx.Count == 0 && ly.Count == 0,
+                (Operator ox, Operator oy) => ox.Symbol.Equals(oy.Symbol),
+                _ => false // e.g. Conjunction, Disjunction, Equality, Equivalence, ExistentialQuantification, Function, Implication, Negation, Predicate, UniversalQuantification..
+            };
+        }
 
-////        private bool TryUnify(Conjunction x, Conjunction y, IDictionary<Variable, Term> unifier)
-////        {
-////            // BUG: Order shouldn't matter (but need to be careful about partially updating unifier)
-////            // perhaps Low and High (internal) props in conjunction?
-////            return TryUnify(x.Left, y.Left, unifier) && TryUnify(x.Right, y.Right, unifier);
-////        }
+        private static bool IsVariable(object x) => x is VariableReference;
 
-////        private bool TryUnify(Disjunction x, Disjunction y, IDictionary<Variable, Term> unifier)
-////        {
-////            // BUG: Order shouldn't matter (but need to be careful about partially updating unifier)
-////            // perhaps Low and High (internal) props in conjunction?
-////            return TryUnify(x.Left, y.Left, unifier) && TryUnify(x.Right, y.Right, unifier);
-////        }
+        private static bool IsCompound(object x)
+        {
+            return x switch
+            {
+                Conjunction => true,
+                Disjunction => true,
+                Equality => true,
+                Equivalence => true,
+                ExistentialQuantification => true,
+                Function => true,
+                Implication => true,
+                Negation => true,
+                Predicate => true,
+                UniversalQuantification => true,
+                _ => false // e.g. Constant, VariableReference or List<object>..
+            };
+        }
 
-////        private bool TryUnify(Equality x, Equality y, IDictionary<Variable, Term> unifier)
-////        {
-////            // BUG: Order shouldn't matter (but need to be careful about partially updating unifier)
-////            // perhaps Low and High (internal) props in conjunction?
-////            return TryUnify(x.Left, y.Left, unifier) && TryUnify(x.Right, y.Right, unifier);
-////        }
+        private static object OperatorOf(object x)
+        {
+            return new Operator(x switch
+            {
+                Conjunction => "∧",
+                Disjunction => "∨",
+                Equality => "=",
+                Equivalence => "⇔",
+                ExistentialQuantification => "∃",
+                Function f => f.Symbol,
+                Implication => "⇒",
+                Negation => "¬",
+                Predicate p => p.Symbol,
+                UniversalQuantification => "∀",
+                _ => throw new ArgumentException("Arg is not a compound", nameof(x)) // e.g. Constant, VariableReference or List<object>..
+            });
+        }
 
-////        private bool TryUnify(Equivalence x, Equivalence y, IDictionary<Variable, Term> unifier)
-////        {
-////            // BUG: Order shouldn't matter (but need to be careful about partially updating unifier)
-////            // perhaps Low and High (internal) props in conjunction?
-////            return TryUnify(x.Left, y.Left, unifier) && TryUnify(x.Right, y.Right, unifier);
-////        }
+        private static object ArgsOf(object x)
+        {
+            return x switch
+            {
+                Conjunction c => new List<object> { c.Left, c.Right },
+                Disjunction d => new List<object> { d.Left, d.Right },
+                Equality e => new List<object> { e.Left, e.Right },
+                Equivalence e => new List<object> { e.Left, e.Right },
+                ExistentialQuantification eq => eq.Sentence,
+                Function f => f.Arguments.ToList<object>(),
+                Implication i => new List<object> { i.Antecedent, i.Consequent },
+                Negation n => n.Sentence,
+                Predicate p => p.Arguments.ToList<object>(),
+                UniversalQuantification uq => uq.Sentence,
+                _ => throw new ArgumentException("Arg is not a compound", nameof(x)) // e.g. Constant, VariableReference or List<object>..
+            };
+        }
 
-////        ////private Sentence TryUnify(ExistentialQuantification x, ExistentialQuantification y, IDictionary<Variable, Term> unifier)
-////        ////{
-////        ////    var variable = ApplyToVariableDeclaration(existentialQuantification.Variable);
-////        ////    var sentence = ApplyToSentence(existentialQuantification.Sentence);
-////        ////    if (variable != existentialQuantification.Variable || sentence != existentialQuantification.Sentence)
-////        ////    {
-////        ////        return new ExistentialQuantification(variable, sentence);
-////        ////    }
-////        ////
-////        ////    return existentialQuantification;
-////        ////}
+        private static bool IsList(object x) => x is List<object>;
 
-////        private bool TryUnify(Implication x, Implication y, IDictionary<Variable, Term> unifier)
-////        {
-////            return TryUnify(x.Antecedent, y.Antecedent, unifier) && TryUnify(x.Consequent, y.Consequent, unifier);
-////        }
+        private static object FirstOf(object x)
+        {
+            if (x is not List<object> l)
+            {
+                throw new ArgumentException("Arg is not a list");
+            }
 
-////        private bool TryUnify(Negation x, Negation y, IDictionary<Variable, Term> unifier)
-////        {
-////            return TryUnify(x.Sentence, y.Sentence, unifier);
-////        }
+            return l.FirstOrDefault();
+        }
 
-////        private bool TryUnify(Predicate x, Predicate y, IDictionary<Variable, Term> unifier)
-////        {
-////            if (!x.Symbol.Equals(y.Symbol))
-////            {
-////                return false;
-////            }
+        private static object RestOf(object x)
+        {
+            if (x is not List<object> l)
+            {
+                throw new ArgumentException("Arg is not a list");
+            }
 
-////            foreach (var args in x.Arguments.Zip(y.Arguments, (x, y) => (x, y)))
-////            {
-////                if (!TryUnify(args.x, args.y, unifier))
-////                {
-////                    return false;
-////                }
-////            }
+            return l.Skip(1).ToList();
+        }
 
-////            return true;
-////        }
+        // NB: we use the algorithm exactly as listed in the book, not even subsituting in a thrown exception
+        // for the "failure" value - the book plays it a little fast and loose with types - i guess assuming
+        // a dynamically typed system (but why then does it say that theta is a substitation..).
+        // ..anyway, we create our own "substitution" type so that we can exactly copy the algorithm as listed
+        // in the book
+        public class Substitution
+        {
+            public object this[object x]
+            {
+                get => Mapping[x];
+                set => Mapping[x] = value;
+            }
 
-////        ////private bool TryUnify(UniversalQuantification x, UniversalQuantification y, IDictionary<Variable, Term> unifier)
-////        ////{
-////        ////    var variable = ApplyToVariableDeclaration(universalQuantification.Variable);
-////        ////    var sentence = ApplyToSentence(universalQuantification.Sentence);
-////        ////    if (variable != universalQuantification.Variable || sentence != universalQuantification.Sentence)
-////        ////    {
-////        ////        return new UniversalQuantification(variable, sentence);
-////        ////    }
-////        ////
-////        ////    return universalQuantification;
-////        ////}
+            public IDictionary<object, object> Mapping { get; } = new Dictionary<object, object>();
 
-////        private bool TryUnify(Term x, Term y, IDictionary<Variable, Term> unifier)
-////        {
-////            return (x, y) switch
-////            {
-////                (Variable variable, _) => TryUnify(variable, y, unifier),
-////                (_, Variable variable) => TryUnify(variable, x, unifier),
-////                (Function functionX, Function functionY) => TryUnify(functionX, functionY, unifier),
-////                _ => x.Equals(y), // only potential for equality is if they're both constants. Worth being explicit?
-////            };
-////        }
+            public bool TryGetValue(object x, out object val) => Mapping.TryGetValue(x, out val);
 
-////        private bool TryUnify(Variable variable, Term other, IDictionary<Variable, Term> unifier)
-////        {
-////            if (unifier.TryGetValue(variable, out var value))
-////            {
-////                return TryUnify(other, value, unifier);
-////            }
-////            else if (other is Variable otherVariable && unifier.TryGetValue(otherVariable, out value))
-////            {
-////                return TryUnify(variable, value, unifier);
-////            }
-////            ////else if (Occurs(variable, other))
-////            ////{
-////            ////    return false;
-////            ////}
-////            else
-////            {
-////                unifier[variable] = other;
-////                return true;
-////            }
-////        }
+            public Substitution Add(object key, object value)
+            {
+                var s = new Substitution();
+                foreach (var kvp in Mapping)
+                {
+                    s[kvp.Key] = kvp.Value;
+                }
 
-////        private bool TryUnify(Function x, Function y, IDictionary<Variable, Term> unifier)
-////        {
-////            if (!x.Symbol.Equals(y.Symbol))
-////            {
-////                return false;
-////            }
+                s[key] = value;
 
-////            foreach (var args in x.Arguments.Zip(y.Arguments, (x, y) => (x, y)))
-////            {
-////                if (!TryUnify(args.x, args.y, unifier))
-////                {
-////                    return false;
-////                }
-////            }
+                return s;
+            }
 
-////            return true;
-////        }
+            public static Substitution Failure = new Substitution();
+        }
 
-////        // NB: we use the algorithm exactly as listed in the book, not even subsituting in a thrown exception
-////        // for the "failure" value - the book plays it a little fast and loose with types - i guess assuming
-////        // a dynamically typed system (but why then does it say that theta is a substitation..).
-////        // ..anyway, we create our own "substitution" type so that we can exactly copy the algorithm as listed
-////        // in the book
-////        private class Substitution
-////        {
-////            private readonly IDictionary<Variable, Term> variables;
+        private class Operator
+        {
+            public Operator(object symbol) => Symbol = symbol;
 
-////            private Substitution(bool isFailure)
-////            {
-////                IsFailure = isFailure;
-////                variables = new Dictionary<Variable, Term>();
-////            }
+            public object Symbol { get; }
 
-////            public IDictionary<Variable, Term> Mapping => !IsFailure ? variables : throw new InvalidOperationException();
-
-////            public bool IsFailure { get; }
-////        }
-////    }
-////}
+            public override bool Equals(object obj) => obj is Operator other && Symbol.Equals(other.Symbol);
+        }
+    }
+}
---
+++ src/SCFirstOrderLogic.Benchmarks/SentenceUnification.cs
@@ -0,0 +1,124 @@
+﻿using BenchmarkDotNet.Attributes;
+using FluentAssertions;
+using SCFirstOrderLogic.SentenceManipulation;
+using System.Collections.Generic;
+using AltSentenceUnifier = SCFirstOrderLogic.Benchmarks.AlternativeImplementations.FromAiAModernApproach.SentenceUnifier;
+
+namespace SCFirstOrderLogic.Benchmarks
+{
+    [MemoryDiagnoser]
+    [InProcess]
+    public class SentenceUnification
+    {
+        private static Function Mother(Term child) => new("Mother", child);
+        private static Predicate Knows(Term knower, Term known) => new("Knows", knower, known);
+        private static readonly Constant john = new("John");
+        private static readonly Constant jane = new("Jane");
+        private static readonly VariableDeclaration x = new("x");
+        private static readonly VariableDeclaration y = new("y");
+
+        [Benchmark]
+        public void Unify1()
+        {
+            var sentence1 = Knows(john, x);
+            var sentence2 = Knows(john, jane);
+
+            new SentenceUnifier().TryUnify(sentence1, sentence2, out var unifier);
+
+            unifier
+                .Should()
+                .Equal(new Dictionary<VariableReference, Term>()
+                {
+                    [x] = jane,
+                });
+        }
+
+        [Benchmark]
+        public void AltUnify1()
+        {
+            var sentence1 = Knows(john, x);
+            var sentence2 = Knows(john, jane);
+
+            AltSentenceUnifier
+                .Unify(sentence1, sentence2, null)
+                .Mapping
+                .Should()
+                .Equal(new Dictionary<object, object>()
+                {
+                    [(VariableReference)x] = jane,
+                });
+        }
+
+        [Benchmark]
+        public void Unify2()
+        {
+            var sentence1 = Knows(john, x);
+            var sentence2 = Knows(y, jane);
+
+            new SentenceUnifier().TryUnify(sentence1, sentence2, out var unifier);
+
+            unifier
+                .Should()
+                .Equal(new Dictionary<VariableReference, Term>()
+                {
+                    [x] = jane,
+                    [y] = john,
+                });
+        }
+
+        [Benchmark]
+        public void AltUnify2()
+        {
+            var sentence1 = Knows(john, x);
+            var sentence2 = Knows(y, jane);
+
+            AltSentenceUnifier
+                .Unify(sentence1, sentence2, null)
+                .Mapping
+                .Should()
+                .Equal(new Dictionary<object, object>()
+                {
+                    [(VariableReference)x] = jane,
+                    [(VariableReference)y] = john,
+                });
+        }
+
+        [Benchmark]
+        public void Unify3()
+        {
+            var sentence1 = Knows(john, x);
+            var sentence2 = Knows(y, Mother(y));
+
+            new SentenceUnifier().TryUnify(sentence1, sentence2, out var unifier);
+
+            unifier
+                .Should()
+                .Equal(new Dictionary<VariableReference, Term>()
+                {
+                    // Book says that x should be Mother(john), but that's not what the algorithm they give
+                    // produces. Easy enough to resolve (after all, y is john), but waiting and seeing how it pans out through usage..
+                    [x] = Mother(y),
+                    [y] = john,
+                });
+        }
+
+        [Benchmark]
+        public void AltUnify3()
+        {
+            var sentence1 = Knows(john, x);
+            var sentence2 = Knows(y, Mother(y));
+
+            AltSentenceUnifier
+                .Unify(sentence1, sentence2, null)
+                .Mapping
+                .Should()
+                .Equal(new Dictionary<object, object>()
+                {
+                    // Book says that x should be Mother(john), but that's not what the algorithm they give
+                    // produces. Easy enough to resolve (after all, y is john), but waiting and seeing how it pans out through usage..
+                    [(VariableReference)x] = Mother(y),
+                    [(VariableReference)y] = john,
+                });
+        }
+    }
+}
