97d47189ca1258d00670cc9786a929800cce5dc5
Simon Condon <simoncondon@hotmail.com>
2022-05-29 17:21:13 +0100

    Initial ListClauseStore tests.

---
+++ src/SCFirstOrderLogic.Tests/Inference/Resolution/ListClauseStoreTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using FlUnit;
+using SCFirstOrderLogic.SentenceManipulation;
+using System.Linq;
+
+namespace SCFirstOrderLogic.Inference.Resolution
+{
+    public static class ListClauseStoreTests
+    {
+        public static Test Smoke => TestThat
+            .Given(() =>
+            {
+                var store = new ListClauseStore();
+                store.AddAsync(new CNFClause(new Predicate("A"))).Wait();
+                store.AddAsync(new CNFClause(new Predicate("B"))).Wait();
+                return store;
+            })
+            .When(store => store.ToArrayAsync().Result)
+            .ThenReturns((_, a) => a.Should().BeEquivalentTo(new[] { new CNFClause(new Predicate("A")), new CNFClause(new Predicate("B")) }));
+
+        public static Test Concurrency_AddDuringEnum => TestThat
+            .Given(() =>
+            {
+                // we have a store that has had a couple of clauses added...
+                var store = new ListClauseStore();
+                store.AddAsync(new CNFClause(new Predicate("A"))).Wait();
+                store.AddAsync(new CNFClause(new Predicate("B"))).Wait();
+
+                // ..then has had an enumeration strarted but not completed..
+                var enumerator = store.GetAsyncEnumerator();
+                enumerator.MoveNextAsync().AsTask().Wait();
+
+                // ..then has had another clause added.
+                store.AddAsync(new CNFClause(new Predicate("C"))).Wait();
+
+                return new { store, enumerator };
+            })
+            .When(g => g.enumerator.MoveNextAsync().AsTask().Wait())
+            .ThenReturns();
+    }
+}
