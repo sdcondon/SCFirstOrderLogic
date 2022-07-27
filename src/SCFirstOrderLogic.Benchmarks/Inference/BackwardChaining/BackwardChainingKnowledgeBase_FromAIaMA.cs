cc04e6d038a47c93423f82b9c18c057224d916e9
Simon Condon <simoncondon@hotmail.com>
2022-07-27 22:23:53 +0100

    Added a reference backward chaining implementation - copied more or less verbatim from "AI: A Modern Approach"

---
+++ src/SCFirstOrderLogic.Benchmarks/Inference/BackwardChaining/BackwardChainingKnowledgeBase_FromAIaMA.cs
@@ -0,0 +1,145 @@
+﻿using SCFirstOrderLogic.Inference;
+using SCFirstOrderLogic.Inference.Unification;
+using SCFirstOrderLogic.SentenceManipulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SCFirstOrderLogic.Benchmarks.Inference.BackwardChaining
+{
+    /// <summary>
+    /// An implementation of <see cref="IKnowledgeBase"/> that uses a backward chaining algorithm. This one is implemented as close as possible to the
+    /// implementation in figure 9.6 of "Artificial Intelligence: A Modern Approach" - for reference purposes.
+    /// </summary>
+    public class BackwardChainingKnowledgeBase_FromAIaMA : IKnowledgeBase
+    {
+        private readonly Dictionary<object, List<CNFClause>> clausesByConsequentSymbol = new ();
+
+        /// <inheritdoc />
+        public Task TellAsync(Sentence sentence, CancellationToken cancellationToken = default)
+        {
+            var cnfSentence = new CNFSentence(sentence);
+
+            if (cnfSentence.Clauses.Any(c => !c.IsDefiniteClause))
+            {
+                throw new ArgumentException("This knowledge base supports only knowledge in the form of definite clauses", nameof(sentence));
+            }
+
+            foreach (var clause in cnfSentence.Clauses)
+            {
+                var consequentSymbol = clause.Literals.Single(l => l.IsPositive).Predicate.Symbol;
+
+                if (!clausesByConsequentSymbol.TryGetValue(consequentSymbol, out var clausesWithThisConsequentSymbol))
+                {
+                    clausesWithThisConsequentSymbol = clausesByConsequentSymbol[consequentSymbol] = new List<CNFClause>();
+                }
+
+                clausesWithThisConsequentSymbol.Add(clause);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        async Task<IQuery> IKnowledgeBase.CreateQueryAsync(Sentence sentence, CancellationToken cancellationToken)
+        {
+            return await CreateQueryAsync(sentence, cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public Task<Query> CreateQueryAsync(Sentence query, CancellationToken cancellationToken = default)
+        {
+            if (query is not Predicate p)
+            {
+                throw new ArgumentException("This knowledge base supports only queries that are propositions");
+            }
+
+            // Doesn't hurt to not standardise here - wont clash because all of the KB rules *are* standardised
+            // (assuming the symbols in the query don't have weird equality rules)..
+            // ..and in any case our standardisation logic assumes all variables to be quantified, otherwise it crashes..
+            //var standardisation = new VariableStandardisation(query);
+            //p = (Predicate)standardisation.ApplyTo(p);
+
+            return Task.FromResult(new Query(p, clausesByConsequentSymbol));
+        }
+
+        /// <summary>
+        /// Query implementation used by <see cref="BackwardChainingKnowledgeBase_FromAIaMA"/>.
+        /// </summary>
+        public class Query : IQuery
+        {
+            private readonly Predicate query;
+            private readonly IReadOnlyDictionary<object, List<CNFClause>> clausesByConsequentSymbol;
+
+            internal Query(Predicate query, IReadOnlyDictionary<object, List<CNFClause>> clausesByConsequentSymbol)
+            {
+                this.query = query;
+                this.clausesByConsequentSymbol = clausesByConsequentSymbol;
+            }
+
+            /// <inheritdoc />
+            public bool IsComplete { get; private set; }
+
+            /// <inheritdoc />
+            public bool Result { get; private set; }
+
+            public IEnumerable<VariableSubstitution> Substitutions { get; private set; }
+
+            /// <inheritdoc />
+            public void Dispose()
+            {
+            }
+
+            /// <inheritdoc />
+            public Task<bool> ExecuteAsync(CancellationToken cancellationToken = default)
+            {
+                Substitutions = FOL_BC_OR(query, new VariableSubstitution());
+
+                Result = Substitutions.Count() > 0;
+                IsComplete = true;
+
+                return Task.FromResult(Result);
+            }
+
+            private IEnumerable<VariableSubstitution> FOL_BC_OR(Predicate goal, VariableSubstitution θ)
+            {
+                foreach (var clause in clausesByConsequentSymbol[goal.Symbol])
+                {
+                    var lhs = clause.Literals.Where(l => l.IsNegated).Select(l => l.Predicate);
+                    var rhs = clause.Literals.Single(l => l.IsPositive);
+                    var unifier = new VariableSubstitution(θ);
+
+                    if (LiteralUnifier.TryUpdate(rhs, goal, unifier))
+                    {
+                        foreach (var θ2 in FOL_BC_AND(lhs, unifier))
+                        {
+                            yield return θ2;
+                        }
+                    }
+                }
+            }
+
+            private IEnumerable<VariableSubstitution> FOL_BC_AND(IEnumerable<Predicate> goals, VariableSubstitution θ)
+            {
+                if (!goals.Any())
+                {
+                    yield return θ;
+                }
+                else
+                {
+                    var first = goals.First();
+                    var rest = goals.Skip(1);
+                    foreach (var θ2 in FOL_BC_OR(θ.ApplyTo(first).Predicate, θ))
+                    {
+                        foreach (var θ3 in FOL_BC_AND(rest, θ2))
+                        {
+                            yield return θ3;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/SCFirstOrderLogic.Tests/Benchmarks/Inference/BackwardChaining/BackwardChainingKnowledgeBase_FromAIaMATests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using FlUnit;
+using SCFirstOrderLogic.ExampleDomains.AiAModernApproach.Chapter9;
+using SCFirstOrderLogic.Inference;
+using SCFirstOrderLogic.SentenceManipulation;
+using System.Collections.Generic;
+using System.Linq;
+using static SCFirstOrderLogic.ExampleDomains.AiAModernApproach.Chapter9.CrimeDomain;
+
+namespace SCFirstOrderLogic.Benchmarks.Inference.BackwardChaining
+{
+    public static class BackwardChainingKnowledgeBase_FromAIaMATests
+    {
+        public static Test CrimeDomainExample => TestThat
+            .Given(() =>
+            {
+                var kb = new BackwardChainingKnowledgeBase_FromAIaMA();
+                kb.Tell(CrimeDomain.Axioms);
+
+                return kb.CreateQueryAsync(IsCriminal(West)).Result;
+            })
+            .When(query => query.Execute())
+            .ThenReturns()
+            .And((_, rv) => rv.Should().BeTrue())
+            .And((query, _) => query.Result.Should().BeTrue());
+
+        public static Test BasicExample => TestThat
+            .Given(() =>
+            {
+                static Predicate IsPerson(Term term) => new Predicate(nameof(IsPerson), term);
+
+                var kb = new BackwardChainingKnowledgeBase_FromAIaMA();
+                kb.Tell(IsPerson(new Constant("John")));
+                kb.Tell(IsPerson(new Constant("Richard")));
+
+                return kb.CreateQueryAsync(IsPerson(new VariableReference("x"))).Result;
+            })
+            .When(query => query.Execute())
+            .ThenReturns()
+            .And((_, rv) => rv.Should().BeTrue())
+            .And((query, _) => query.Result.Should().BeTrue())
+            .And((query, _) => query.Substitutions.Select(s => s.Bindings).Should().BeEquivalentTo(new Dictionary<VariableReference, Term>[]
+            {
+                new() { [new VariableReference("x")] = new Constant("John") },
+                new() { [new VariableReference("x")] = new Constant("Richard") },
+            }, opts => opts.RespectingRuntimeTypes()));
+    }
+}
