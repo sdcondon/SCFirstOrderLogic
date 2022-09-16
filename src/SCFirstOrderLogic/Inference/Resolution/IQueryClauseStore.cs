42447dae5646b8f97e9cfb9e39a522f2430460c8
Simon Condon <simoncondon@hotmail.com>
2022-09-16 17:11:01 +0100

    doc tweak

--- src/SCFirstOrderLogic/Inference/Resolution/IQueryClauseStore.cs
+++ src/SCFirstOrderLogic/Inference/Resolution/IQueryClauseStore.cs
@@ -4,7 +4,7 @@ namespace SCFirstOrderLogic.Inference.Resolution
 {
     /// <summary>
     /// Sub-type of <see cref="IClauseStore"/> for types intended for storing the intermediate clauses of a particular query.
-    /// Notably, is <see cref="IDisposable"/> so that any required resources can be promptly tidied away once the query is complete.
+    /// Notably, is <see cref="IDisposable"/> so that any unmanaged resources can be promptly tidied away once the query is complete.
     /// </summary>
     public interface IQueryClauseStore : IClauseStore, IDisposable
     {
