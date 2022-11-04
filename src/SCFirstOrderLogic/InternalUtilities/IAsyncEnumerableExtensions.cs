49bc6a64323e20d3300b8b7c93d9ad8c44e61495
Simon Condon <simoncondon@hotmail.com>
2022-11-04 13:15:46 +0000

    Added `AnyAsync` method to `IAsyncEnumerableExtensions` - useful for clause store support in `SimpleForwardChainingKnowledgeBase`.

--- src/SCFirstOrderLogic/InternalUtilities/IAsyncEnumerableExtensions.cs
+++ src/SCFirstOrderLogic/InternalUtilities/IAsyncEnumerableExtensions.cs
@@ -6,6 +6,9 @@ namespace SCFirstOrderLogic.InternalUtilities
 {
     /// <summary>
     /// Extension methods for <see cref="IAsyncEnumerable{T}"/> instances.
+    /// <para/>
+    /// NB: Yes, we could take a dependency on System.Linq.Async instead, but I quite
+    /// like the fact that this package has no third-party dependencies.
     /// </summary>
     internal static class IAsyncEnumerableExtensions
     {
@@ -38,6 +41,30 @@ namespace SCFirstOrderLogic.InternalUtilities
             }
 
             return list;
+        }
+
+        /// <summary>
+        /// Asynchronously determines if a given <see cref="IAsyncEnumerable{T}"/> contains any elements.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the enumerable in question.</typeparam>
+        /// <param name="asyncEnumerable">The enumerable to examine.</param>
+        /// <param name="cancellationToken">A cancellation token for the operation.</param>
+        /// <returns>A task that returns true if and only if the given enumerable contains any elements.</returns>
+        public static async Task<bool> AnyAsync<T>(this IAsyncEnumerable<T> asyncEnumerable, CancellationToken cancellationToken = default)
+        {
+            IAsyncEnumerator<T>? enumerator = null;
+            try
+            {
+                enumerator = asyncEnumerable.GetAsyncEnumerator(cancellationToken);
+                return await enumerator.MoveNextAsync();
+            }
+            finally
+            {
+                if (enumerator != null)
+                {
+                    await enumerator.DisposeAsync();
+                }
+            }
         }
     }
 }
