3fbeee705ad55848fa056cdec1d0fe6d77727801
Simon Condon <simoncondon@hotmail.com>
2023-07-09 11:57:14 +0100

    Dropped another V5 todo that I have changed my mind on - ICustomFormatter et al isn't the right call for formatting of sentences (what kind of format strings would we support?) - the current implementation is fine.

--- src/SCFirstOrderLogic/SentenceFormatting/SentenceFormatter.cs
+++ src/SCFirstOrderLogic/SentenceFormatting/SentenceFormatter.cs
@@ -6,15 +6,10 @@ using System.Linq;
 namespace SCFirstOrderLogic.SentenceFormatting
 {
     /// <summary>
-    /// <para>
     /// This class provides functionality for rendering <see cref="Sentence"/> instances (and <see cref="CNFSentence"/> instances) in the standard first-order logic syntax.
     /// Using a single <see cref="SentenceFormatter"/> instance allows for unique (and customisable) labelling of standardised variables and Skolem functions for all sentences formatted with the instance.
-    /// </para>
-    /// <para>
-    /// TODO-V5: This implementation will suffice while I figure out what I want (IFormatProvider and ICustomFormatter/s, probably).
-    /// Will also ultimately want something that is more intelligent with brackets (i.e. drops them where not needed), too. 
-    /// </para>
     /// </summary>
+    // TODO: Will ultimately want something that is more intelligent with brackets (i.e. drops them where not needed). 
     public class SentenceFormatter
     {
         private readonly ILabellingScope<StandardisedVariableIdentifier> standardisedVariableLabellingScope;
