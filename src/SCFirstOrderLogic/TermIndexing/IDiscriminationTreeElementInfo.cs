1bd90696addf4c7870eba1bcde3120b0ddd1b8a0
Simon Condon <simoncondon@hotmail.com>
2023-08-13 10:59:32 +0100

    Push a few record types into the same source file. Don't generally like doing this, but in this case I think its acceptable.

--- src/SCFirstOrderLogic/TermIndexing/IDiscriminationTreeElementInfo.cs
+++ src/SCFirstOrderLogic/TermIndexing/IDiscriminationTreeElementInfo.cs
@@ -15,4 +15,38 @@ namespace SCFirstOrderLogic.TermIndexing
         /// </summary>
         int ChildElementCount { get; }
     }
+
+    /// <summary>
+    /// Information about a function, for storage against a node of a discrimination tree.
+    /// </summary>
+    /// <param name="Identifier">The identifier of the represented function.</param>
+    /// <param name="ArgumentCount">The number of arguments of the represented function.</param>
+    public sealed record DiscriminationTreeFunctionInfo(object Identifier, int ArgumentCount) : IDiscriminationTreeElementInfo
+    {
+        /// <inheritdoc/>
+        public int ChildElementCount => ArgumentCount;
+    }
+
+    /// <summary>
+    /// Information about a variable, for storage against a node of a discrimination tree.
+    /// </summary>
+    /// <param name="Ordinal">
+    /// The ordinal of the represented variable - that is, the index of its position in a list of variables that
+    /// exist in the term, in the order in which they are first encountered by a depth-first traversal.
+    /// </param>
+    public sealed record DiscriminationTreeVariableInfo(int Ordinal) : IDiscriminationTreeElementInfo
+    {
+        /// <inheritdoc/>
+        public int ChildElementCount => 0;
+    }
+
+    /// <summary>
+    /// Information about a constant, for storage against a node of a discrimination tree.
+    /// </summary>
+    /// <param name="Identifier">The identifier of the represented constant.</param>
+    public sealed record DiscriminationTreeConstantInfo(object Identifier) : IDiscriminationTreeElementInfo
+    {
+        /// <inheritdoc/>
+        public int ChildElementCount => 0;
+    }
 }
