387c44c70ba616f8d1b1921b1e6d8ee449260db4
Simon Condon <simoncondon@hotmail.com>
2024-06-18 21:33:30 +0100

    removed now-unneeded using

--- src/SCFirstOrderLogic/Inference/IQueryExtensions.cs
+++ src/SCFirstOrderLogic/Inference/IQueryExtensions.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) 2021-2024 Simon Condon.
 // You may use this file in accordance with the terms of the MIT license.
-using System.Runtime.CompilerServices;
-
 namespace SCFirstOrderLogic.Inference;
 
 /// <summary>
