8f3b4f8323cd8a526ac16f9c52f1505644f9fd86
Simon Condon <simoncondon@hotmail.com>
2024-07-25 15:49:07 +0100

    Minor xml doc tweak

--- src/SCFirstOrderLogic/SentenceFormatting/(Labellers)/LabelSets.cs
+++ src/SCFirstOrderLogic/SentenceFormatting/(Labellers)/LabelSets.cs
@@ -10,17 +10,17 @@ namespace SCFirstOrderLogic.SentenceFormatting;
 public static class LabelSets
 {
     /// <summary>
-    /// The (lowercase) Greek alphabet.
+    /// The (lower case) Greek alphabet.
     /// </summary>
     public static readonly IEnumerable<string> LowerGreekAlphabet = new[] { "α", "β", "γ", "δ", "ε", "ζ", "η", "θ", "ι", "κ", "λ", "μ", "ν", "ξ", "ο", "π", "ρ", "σ", "τ", "υ", "φ", "χ", "ψ", "ω" };
 
     /// <summary>
-    /// The (lower-case) modern Latin alphabet
+    /// The (lower case) modern Latin alphabet
     /// </summary>
     public static readonly IEnumerable<string> LowerModernLatinAlphabet = new[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
     /// <summary>
-    /// The (upper-case) modern Latin alphabet
+    /// The (upper case) modern Latin alphabet
     /// </summary>
     public static readonly IEnumerable<string> UpperModernLatinAlphabet = new[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 }
