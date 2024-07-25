a00a605a62dbc0bc50925a7b4beed46f01e58c39
Simon Condon <simoncondon@hotmail.com>
2024-07-25 23:12:30 +0100

    Deleted needless waffle from XML documentation of EqualityIdentifier.

--- src/SCFirstOrderLogic/EqualityIdentifier.cs
+++ src/SCFirstOrderLogic/EqualityIdentifier.cs
@@ -3,22 +3,8 @@
 namespace SCFirstOrderLogic;
 
 /// <summary>
-/// <para>
 /// Singleton representation of the identifier of the equality predicate. In typical FOL syntax, this is written as:
 /// <code>{term} = {term}</code>
-/// </para>
-/// <para>
-/// NB: Equality is just a binary predicate with particular (admittedly fairly special combination of - given that they mean that the predicate
-/// describes a partition of the terms of the domain) properties - namely commutativity, transitivity &amp; reflexivity.
-/// Not having a separate <see cref="Sentence"/> (or indeed <see cref="Predicate"/>) sub-type for it thus makes sense - and in fact makes
-/// a bunch of stuff to do with normalisation easier because it means there's a singular <see cref="Sentence"/> subtype for atomic sentences.
-/// </para>
-/// <para>
-/// This does of course mean that the way we render these in FoL syntax ("Equals(x, y)") doesn't match the usual FoL syntax for equality
-/// ("x = y"). Easy enough to resolve, either specifically (by accounting specifically for this identifier in formatting logic) or in general (an IPredicateIdentifier 
-/// interface with an IsRenderedInfix property?). Haven't done that yet because its not a big deal, is not worth the complexity, and I'm not yet sure
-/// how I want to deal with rendering in FoL syntax going forwards.
-/// </para>
 /// </summary>
 public sealed class EqualityIdentifier
 {
