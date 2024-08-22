35f01224a53cfe3e9c613609051c30e62009cf27
Simon Condon <simoncondon@hotmail.com>
2024-08-22 18:48:11 +0100

    removed unused using directives

--- src/SCFirstOrderLogic/CNFSentence.cs
+++ src/SCFirstOrderLogic/CNFSentence.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) 2021-2024 Simon Condon.
 // You may use this file in accordance with the terms of the MIT license.
 using SCFirstOrderLogic.SentenceFormatting;
-using SCFirstOrderLogic.SentenceManipulation;
-using SCFirstOrderLogic.SentenceManipulation.Normalisation;
 using System;
 using System.Collections.Generic;
 
