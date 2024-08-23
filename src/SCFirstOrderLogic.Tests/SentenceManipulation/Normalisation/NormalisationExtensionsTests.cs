877dd732825e5c8006d91f9dcc94e20551cff5c1
Simon Condon <simoncondon@hotmail.com>
2024-08-23 13:09:40 +0100

    Oops, test fix.

---
+++ src/SCFirstOrderLogic.Tests/SentenceManipulation/Normalisation/NormalisationExtensionsTests.cs
@@ -0,0 +1,168 @@
+﻿using FluentAssertions;
+using FlUnit;
+using static SCFirstOrderLogic.SentenceCreation.SentenceFactory;
+
+namespace SCFirstOrderLogic.SentenceManipulation.Normalisation;
+
+public static class NormalisationExtensionsTests
+{
+    private static Sentence P => new Predicate("P");
+    private static Sentence Q => new Predicate("Q");
+    private static Sentence R => new Predicate("R");
+
+    public static Test ToCNFBehaviour => TestThat
+        .GivenEachOf(() => new[]
+        {
+            new
+            {
+                Sentence = P,
+                ExpectedCNF = new
+                {
+                    Clauses = new[]
+                    {
+                        new
+                        {
+                            IsDefiniteClause = true,
+                            IsGoalClause = false,
+                            IsHornClause = true,
+                            IsUnitClause = true,
+                            Literals = new[]
+                            {
+                                new { Predicate = P, IsNegated = false },
+                            }
+                        }
+                    }
+                }
+            },
+            new
+            {
+                Sentence = Not(P),
+                ExpectedCNF = new
+                {
+                    Clauses = new[]
+                    {
+                        new
+                        {
+                            IsDefiniteClause = false,
+                            IsGoalClause = true,
+                            IsHornClause = true,
+                            IsUnitClause = true,
+                            Literals = new[]
+                            {
+                                new { Predicate = P, IsNegated = true },
+                            }
+                        }
+                    }
+                }
+            },
+            new
+            {
+                Sentence = Iff(P, Not(And(Not(Q), Not(R)))),
+                ExpectedCNF = new
+                {
+                    Clauses = new[]
+                    {
+                        new
+                        {
+                            IsDefiniteClause = false,
+                            IsGoalClause = false,
+                            IsHornClause = false,
+                            IsUnitClause = false,
+                            Literals = new[]
+                            {
+                                new { Predicate = P, IsNegated = true },
+                                new { Predicate = Q, IsNegated = false },
+                                new { Predicate = R, IsNegated = false },
+                            }
+                        },
+                        new
+                        {
+                            IsDefiniteClause = true,
+                            IsGoalClause = false,
+                            IsHornClause = true,
+                            IsUnitClause = false,
+                            Literals = new[]
+                            {
+                                new { Predicate = P, IsNegated = false },
+                                new { Predicate = Q, IsNegated = true }
+                            }
+                        },
+                        new
+                        {
+                            IsDefiniteClause = true,
+                            IsGoalClause = false,
+                            IsHornClause = true,
+                            IsUnitClause = false,
+                            Literals = new[]
+                            {
+                                new { Predicate = P, IsNegated = false },
+                                new { Predicate = R, IsNegated = true }
+                            }
+                        }
+                    }
+                }
+            },
+            new
+            {
+                // BUG?: We don't remove the trivially true clauses (e.g. P ∨ ¬P) - none of the source material
+                // references this as being part of the normalisation process. But we probably should..
+                Sentence = Or(And(P, Q), And(Not(P), Not(Q))),
+                ExpectedCNF = new
+                {
+                    Clauses = new[]
+                    {
+                        new
+                        {
+                            IsDefiniteClause = true,
+                            IsGoalClause = false,
+                            IsHornClause = true,
+                            IsUnitClause = false,
+                            Literals = new[]
+                            {
+                                new { Predicate = P, IsNegated = false },
+                                new { Predicate = P, IsNegated = true },
+                            }
+                        },
+                        new
+                        {
+                            IsDefiniteClause = true,
+                            IsGoalClause = false,
+                            IsHornClause = true,
+                            IsUnitClause = false,
+                            Literals = new[]
+                            {
+                                new { Predicate = P, IsNegated = false },
+                                new { Predicate = Q, IsNegated = true },
+                            }
+                        },
+                        new
+                        {
+                            IsDefiniteClause = true,
+                            IsGoalClause = false,
+                            IsHornClause = true,
+                            IsUnitClause = false,
+                            Literals = new[]
+                            {
+                                new { Predicate = Q, IsNegated = false },
+                                new { Predicate = P, IsNegated = true }
+                            }
+                        },
+                        new
+                        {
+                            IsDefiniteClause = true,
+                            IsGoalClause = false,
+                            IsHornClause = true,
+                            IsUnitClause = false,
+                            Literals = new[]
+                            {
+                                new { Predicate = Q, IsNegated = false },
+                                new { Predicate = Q, IsNegated = true }
+                            }
+                        }
+                    }
+                }
+            },
+        })
+        .When(tc => tc.Sentence.ToCNF())
+        .ThenReturns((tc, retVal) => retVal.Should().BeEquivalentTo(tc.ExpectedCNF));
+}
