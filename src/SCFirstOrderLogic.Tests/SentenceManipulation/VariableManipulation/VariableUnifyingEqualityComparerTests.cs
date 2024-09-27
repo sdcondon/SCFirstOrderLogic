f8b051421d63361133d3b020f0a4bc52fe45f647
Simon Condon <simoncondon@hotmail.com>
2024-09-27 19:49:24 +0100

    Removed VariableUnifier, added an equality comparer instead - which is directly usable in our FVI node classes. WIP, impl not quite right.

---
+++ src/SCFirstOrderLogic.Tests/SentenceManipulation/VariableManipulation/VariableUnifyingEqualityComparerTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using FlUnit;
+using SCFirstOrderLogic.SentenceManipulation.Normalisation;
+using System.Linq;
+using static SCFirstOrderLogic.TestProblems.GenericDomainOperableSentenceFactory;
+
+namespace SCFirstOrderLogic.SentenceManipulation.VariableManipulation;
+
+public class VariableUnifyingEqualityComparerTests
+{
+    public static Test EqualityBehaviour_Positive => TestThat
+        .GivenEachOf<EqualityTestCase>(() =>
+        [
+            new(
+                X: CNFClause.Empty,
+                Y: CNFClause.Empty),
+
+            new(
+                X: P(C, F(X)),
+                Y: P(C, F(Y))),
+
+            new(
+                X: P(F(X), F(Y)),
+                Y: P(F(Y), F(X))),
+
+            new( // todo: this shouldn't pass with current implementation..?! meh, tired. tomo..
+                X: P(X, Y, Z),
+                Y: P(Y, Z, X)),
+        ])
+        .When(tc =>
+        {
+            var comparer = new VariableUnifyingEqualityComparer();
+            return (XEqualsY: comparer.Equals(tc.X, tc.Y), YEqualsX: comparer.Equals(tc.Y, tc.X), HashCodeEquality: comparer.GetHashCode(tc.X) == comparer.GetHashCode(tc.Y));
+        })
+        .ThenReturns()
+        .And((_, rv) => rv.XEqualsY.Should().BeTrue())
+        .And((_, rv) => rv.YEqualsX.Should().BeTrue())
+        .And((_, rv) => rv.HashCodeEquality.Should().BeTrue());
+
+    public static Test EqualityBehaviour_Negative => TestThat
+        .GivenEachOf<EqualityTestCase>(() =>
+        [
+            new(
+                X: P(C, X),
+                Y: P(C, D)),
+
+            new (
+                X: P(C, X),
+                Y: P(Y, D)),
+
+            new (
+                X: P(C, X),
+                Y: P(Y, F(Y))),
+
+            new (
+                X: P(X, X),
+                Y: P(Y, C)),
+
+            new (
+                X: P(X, X),
+                Y: P(X, Y)),
+
+            new (
+                X: P(X, X),
+                Y: P(A, B)),
+
+            new (
+                X: P(C, X),
+                Y: P(X, D)),
+
+            new (
+                X: P(C, X),
+                Y: P(C, F(X))),
+
+            new (
+                X: P(X, F(X)),
+                Y: P(G(Y), Y)),
+
+            new (
+                X: P(X) | Q(X),
+                Y: P(X) | Q(Y)),
+        ])
+        .When(tc =>
+        {
+            var comparer = new VariableUnifyingEqualityComparer();
+            return (XEqualsY: comparer.Equals(tc.X, tc.Y), YEqualsX: comparer.Equals(tc.Y, tc.X));
+        })
+        .ThenReturns()
+        .And((tc, rv) => rv.XEqualsY.Should().BeFalse())
+        .And((tc, rv) => rv.YEqualsX.Should().BeFalse());
+
+    private record EqualityTestCase(CNFClause X, CNFClause Y)
+    {
+        public EqualityTestCase(Sentence X, Sentence Y)
+            : this(X.ToCNF().Clauses.Single(), Y.ToCNF().Clauses.Single()) { }
+
+        public EqualityTestCase(CNFClause X, Sentence Y)
+            : this(X, Y.ToCNF().Clauses.Single()) { }
+
+        public EqualityTestCase(Sentence X, CNFClause Y)
+            : this(X.ToCNF().Clauses.Single(), Y) { }
+    }
+}
---
+++ src/SCFirstOrderLogic/SentenceManipulation/VariableManipulation/VariableUnifyingEqualityComparer.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace SCFirstOrderLogic.SentenceManipulation.VariableManipulation;
+
+/// <summary>
+/// <para>
+/// Equality comparer for CNF clauses for which variable identifiers (though not their distinctness) are deemed irrelevant.
+/// That is, an equality comparer that considers P(x, y) equal to P(a, b) (but of course distinct from P(x, x)).
+/// </para>
+/// <para>
+/// NB: of course, such comparison is non-trivial in terms of performance. When an unambiguous ordering of 
+/// literals can be established, consider transformation via <see cref="VariableManipulationExtensions.Ordinalise(Term)"/>
+/// followed by equality comparison using plain old <see cref="object.Equals(object?)"/> instead.
+/// </para>
+/// </summary>
+public class VariableUnifyingEqualityComparer : IEqualityComparer<CNFClause>
+{
+    private static readonly VariableReference VariableReferenceForHashCode = new(new {});
+
+    /// <inheritdoc/>
+    public bool Equals(CNFClause? x, CNFClause? y)
+    {
+        if (x == null)
+        {
+            return y == null;
+        }
+        else if (y == null)
+        {
+            return false;
+        }
+        else
+        {
+            return TryUpdateUnifier(x, y, new MutableVariableSubstitution());
+        }
+    }
+
+    /// <inheritdoc/>
+    public int GetHashCode([DisallowNull] CNFClause obj)
+    {
+        return TransformForHashCode(obj).GetHashCode();
+    }
+
+    private static bool TryUpdateUnifier(CNFClause x, CNFClause y, MutableVariableSubstitution unifier)
+    {
+        if (x.Literals.Count != y.Literals.Count)
+        {
+            return false;
+        }
+
+        foreach (var literals in x.Literals.Zip(y.Literals, (x, y) => (x, y)))
+        {
+            if (!TryUpdateUnifier(literals.x, literals.y, unifier))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool TryUpdateUnifier(Literal x, Literal y, MutableVariableSubstitution unifier)
+    {
+        if (x.IsNegated != y.IsNegated)
+        {
+            return false;
+        }
+
+        return TryUpdateUnifier(x.Predicate, y.Predicate, unifier);
+    }
+
+    private static bool TryUpdateUnifier(Predicate x, Predicate y, MutableVariableSubstitution unifier)
+    {
+        if (!x.Identifier.Equals(y.Identifier)
+            || x.Arguments.Count != y.Arguments.Count)
+        {
+            return false;
+        }
+
+        foreach (var args in x.Arguments.Zip(y.Arguments, (x, y) => (x, y)))
+        {
+            if (!TryUpdateUnifier(args.x, args.y, unifier))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool TryUpdateUnifier(Term x, Term y, MutableVariableSubstitution unifier)
+    {
+        return (x, y) switch
+        {
+            (VariableReference variableX, VariableReference variableY) => TryUpdateUnifier(variableX, variableY, unifier),
+            (Function functionX, Function functionY) => TryUpdateUnifier(functionX, functionY, unifier),
+            _ => false
+        };
+    }
+
+    private static bool TryUpdateUnifier(VariableReference x, VariableReference y, MutableVariableSubstitution unifier)
+    {
+        if (!unifier.Bindings.TryGetValue(x, out var boundXValue))
+        {
+            unifier.AddBinding(x, y);
+        }
+        else if (!boundXValue.Equals(y))
+        {
+            return false;
+        }
+
+        if (!unifier.Bindings.TryGetValue(y, out var boundYValue))
+        {
+            unifier.AddBinding(y, x);
+        }
+        else if (!boundYValue.Equals(x))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static bool TryUpdateUnifier(Function x, Function y, MutableVariableSubstitution unifier)
+    {
+        if (!x.Identifier.Equals(y.Identifier) || x.Arguments.Count != y.Arguments.Count)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < x.Arguments.Count; i++)
+        {
+            if (!TryUpdateUnifier(x.Arguments[i], y.Arguments[i], unifier))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static CNFClause TransformForHashCode(CNFClause clause)
+    {
+        var isChanged = false;
+        var transformed = new List<Literal>(clause.Literals.Count);
+
+        foreach (var literal in clause.Literals)
+        {
+            var transformedLiteral = TransformForHashCode(literal);
+
+            if (!ReferenceEquals(transformedLiteral, literal))
+            {
+                isChanged = true;
+            }
+        }
+
+        if (isChanged)
+        {
+            return new CNFClause(transformed);
+        }
+
+        return clause;
+    }
+
+    private static Literal TransformForHashCode(Literal literal)
+    {
+        var transformedPredicate = TransformForHashCode(literal.Predicate);
+
+        if (ReferenceEquals(transformedPredicate, literal.Predicate))
+        {
+            return literal;
+        }
+
+        return new(transformedPredicate, literal.IsNegated);
+    }
+
+    private static Predicate TransformForHashCode(Predicate predicate)
+    {
+        var isChanged = false;
+        var transformed = new Term[predicate.Arguments.Count];
+
+        for (int i = 0; i < predicate.Arguments.Count; i++)
+        {
+            transformed[i] = TransformForHashCode(predicate.Arguments[i]);
+
+            if (!ReferenceEquals(transformed[i], predicate.Arguments[i]))
+            {
+                isChanged = true;
+            }
+        }
+
+        if (isChanged)
+        {
+            return new Predicate(predicate.Identifier, transformed);
+        }
+
+        return predicate;
+    }
+
+    private static Term TransformForHashCode(Term term) => term switch
+    {
+        VariableReference => VariableReferenceForHashCode,
+        Function function => TransformForHashCode(function),
+        _ => throw new ArgumentException($"Unsupported Term type '{term.GetType()}'", nameof(term))
+    };
+
+    private static Function TransformForHashCode(Function function)
+    {
+        var isChanged = false;
+        var transformed = new Term[function.Arguments.Count];
+
+        for (int i = 0; i < function.Arguments.Count; i++)
+        {
+            transformed[i] = TransformForHashCode(function.Arguments[i]);
+
+            if (!ReferenceEquals(transformed[i], function.Arguments[i]))
+            {
+                isChanged = true;
+            }
+        }
+
+        if (isChanged)
+        {
+            return new Function(function.Identifier, transformed);
+        }
+
+        return function;
+    }
+}
