b09607e29521984107e347abadf6410fd3335b95
Simon Condon <simon@sdcondon.net>
2025-04-12 19:52:43 +0100

    docs site: use blazorise components & respect system color mode pref

--- src/SCFirstOrderLogic.Documentation/Program.cs
+++ src/SCFirstOrderLogic.Documentation/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SCFirstOrderLogic.Documentation.Razor;
@@ -6,6 +9,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons()
+    .AddScoped(sp => new HttpClient
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    });
 
 await builder.Build().RunAsync();
---
+++ src/SCFirstOrderLogic.Documentation/wwwroot/js/startBlazor.js
@@ -0,0 +1,28 @@
+﻿// Shamelessly nabbed and adapted from https://swharden.com/blog/2022-05-29-blazor-loading-progress/
+let loadedCount = 0;
+const resourcesToLoad = [];
+Blazor.start({
+    loadBootResource:
+        function (type, filename, defaultUri, integrity) {
+            if (type == "dotnetjs")
+                return defaultUri;
+
+            const fetchResources = fetch(defaultUri, {
+                cache: 'no-cache',
+                integrity: integrity
+            });
+
+            resourcesToLoad.push(fetchResources);
+
+            fetchResources.then((r) => {
+                loadedCount += 1;
+                if (filename == "blazor.boot.json")
+                    return;
+                const totalCount = resourcesToLoad.length;
+                document.getElementById('progressLabel').innerHTML = `Loading,&nbsp;please&nbsp;wait..&nbsp;(${loadedCount}/${totalCount})`;
+                document.getElementById('progressDetail').innerText = filename;
+            });
+
+            return fetchResources;
+        }
+});
